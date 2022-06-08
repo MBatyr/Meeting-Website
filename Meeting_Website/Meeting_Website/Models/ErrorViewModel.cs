0d950ca084d5f3db382950b154ba64c73de0984e
Baku <bktbkzmbkv@gmail.com>
2022-06-08 19:54:04 +0600

    FirstInitial

---
+++ Meeting_Website/Meeting_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Meeting_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
